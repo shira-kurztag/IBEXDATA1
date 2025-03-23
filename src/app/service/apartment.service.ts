5da3d711b50dfae4f2eb75ad9fb64c6421ce8039
TamarStefansky123 <38215044819@mby.org.il>
2025-03-23 18:33:23 +0200

    msg

---
+++ src/app/service/apartment.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ApartmentDTO } from '../Models/ApartmentDTO';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApartmentService {
+
+  private apiUrl = 'https://localhost:5178/api/Apartment/GetApartmentsByBuilding/2305'; //שליפת דירות לפי ID של בנייין
+  
+  
+   //private apiUrlBase = 'http://localhost:5090/api/Banks'; // URL בסיסי ל-API
+   
+   
+    constructor(private http: HttpClient) {}
+   
+    GetApartmentsByBuilding(buildingId: number): Observable<ApartmentDTO[]> {
+      return this.http.get<ApartmentDTO[]>(this.apiUrl);
+    }
+    
+  }
