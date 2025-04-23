a07675ebbd35e568519f442105f60c5d12e5f812
LeahWaiss <38328120985@mby.co.il>
2025-04-23 16:31:33 +0300

    changes 23\04

--- src/app/service/apartment.service.ts
+++ src/app/service/apartment.service.ts
@@ -1,3 +1,4 @@
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApartmentDTO } from '../Models/ApartmentDTO.model';
@@ -8,9 +9,9 @@ import { Observable } from 'rxjs';
 })
 export class ApartmentService {
  
-  private apiUrl = 'https://localhost:5178/api/ApartmentDetails/GetApartmentsByBuilding/2305'; //שליפת דירות לפי ID של בנייין
+  private apiUrl = 'https://localhost:5178/api/Apartment/GetApartmentsByBuilding/2305'; //שליפת דירות לפי ID של בנייין
  
- 
+  private BASE_URL= 'https://localhost:5178/api/Apartment/2305/add-apartment';
    //private apiUrlBase = 'http://localhost:5090/api/Banks'; // URL בסיסי ל-API
    
    
@@ -19,6 +20,11 @@ export class ApartmentService {
     GetApartmentsByBuilding(buildingId: number): Observable<ApartmentDTO[]> {
       return this.http.get<ApartmentDTO[]>(this.apiUrl);
     }
-   
+    addApartment(item: ApartmentDTO): Observable<ApartmentDTO[]> {
+      var x = this.http.post<ApartmentDTO[]>(this.BASE_URL, item);
+      return x;
+  }
+ 
+ 
   }
  
