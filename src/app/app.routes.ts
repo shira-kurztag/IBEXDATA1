df7b5899268aa3f19e82e4d534c99ec11f455adb
LeahWaiss <38328120985@mby.co.il>
2025-04-23 18:08:53 +0300

    changes sofi day 23_04

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,7 +10,7 @@ import { AddContractorComponent } from "./Component/contractor_/addcontractor/ad
 import { MortagegeComponent } from "./Component/mortagege/mortagege.component";
 import { AddTenantComponent } from "./Component/tenant/addtenant/addtenant.component";
 import { MainTenantComponent } from "./Component/tenant/maintenant/maintenant.component";
-
+import { AddingApartmentComponent } from "./Component/adding-apartment/adding-apartment.component";
 
 
 export const routes: Routes = [
@@ -25,7 +25,8 @@ export const routes: Routes = [
     { path:'listcontractor', component:ListContractorsComponent},
     { path:'contractor/:id', component:MaincontractorComponent},
     { path:'AddContractors', component:AddContractorComponent},
-    { path: 'mortagege', component: MortagegeComponent }
+    { path: 'mortagege', component: MortagegeComponent },
+    { path: 'AddingApartmentComponent', component: AddingApartmentComponent }
  
     // { path: 'contractor', component: ContractorComponent, children:[
     //    // {path:'', redirectTo: 'list', pathMatch:'full'},
