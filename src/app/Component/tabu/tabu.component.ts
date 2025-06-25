717bed2d1c3ee059ce0cb3cbf0d00836a723a8ae
lali <38214803520@mby.co.il>
2025-06-25 12:48:03 +0300

    25.6

--- src/app/Component/tabu/tabu.component.ts
+++ src/app/Component/tabu/tabu.component.ts
@@ -18,6 +18,20 @@ export class TabuComponent {
   @Input() apartmentId!: number;
   editMode: boolean = false;
   srvOwnerService: TabuService = inject(TabuService);
+  isMortgagePaid: boolean = false;
+  isPurchaseTax: boolean = false;
+  isCapitalTax: boolean = false;
+  isApprovalNeededSaleTaxHas: boolean = false;
+  isMunicipalityApproval: boolean = false;
+  isTaxApprovedTransferRightsSource: boolean = false;
+  isPartiesSigned: boolean = false;
+  isSignedByMm: boolean = false;
+  isMortgageBillReceived: boolean = false;
+  isTaxApprovedTransferRightsValid: boolean = false;
+  isMunicipalityApprovalValidity: boolean = false;
+  isApprovalNeededSaleTaxValid: boolean = false;
+  isCapitalTaxValid: boolean = false;
+  isPurchaseTaxValid: boolean = false;
 
 
   constructor(
@@ -32,9 +46,43 @@ export class TabuComponent {
     });
   }
   ngOnInit(): void {
+    this.updateLabels()
     console.log(this.apartmentId);
     this.getTabu();
+     this.tabuForm.valueChanges.subscribe(() => {
+      this.updateLabels();
+      console.log(this.isPurchaseTax);
+      
+    });
   }
+getEmptyTabu(): TabuDTO {
+  return {
+    tabuId: 0,
+    apartmentId: 0,
+    ownerId: 0,
+    isMortgagePaid: false,
+    commonArea: 0,
+    notarizedPoweReason: '',
+    isPurchaseTax: false,
+    isPurchaseTaxValid: false,
+    isCapitalTax: false,
+    isCapitalTaxValid: false,
+    isApprovalNeededSaleTaxHas: false,
+    isApprovalNeededSaleTaxValid: false,
+    isMunicipalityApproval: false,
+    isTaxApprovedTransferRights: false,
+    isTaxApprovedTransferRightsSource: false,
+    isTaxApprovedTransferRightsValid: false,
+    isPartiesSigned: false,
+    isSignedByMm: false,
+    isMortgageBillReceived: false,
+    fareName: '',
+    fareAmount: 0,
+    bloc: 0,
+    smooth: 0,
+    smothArea: 0,
+  };
+}
 
   createTabuForm(tabu: TabuDTO): FormGroup {
     return this.fb.group({
@@ -68,29 +116,38 @@ export class TabuComponent {
     });
   }
 
+  // getTabu(): void {
+  //   this.srvTabu.GetTabuByApartmentId(this.apartmentId).subscribe((tabu: TabuDTO) => {
+  //     if (tabu) {
+  //       const formGroup = this.createTabuForm(tabu);
+  //       this.tabuForm = formGroup;
+  //     }
+  //   });
+  // }
   getTabu(): void {
-    this.srvTabu.GetTabuByApartmentId(this.apartmentId).subscribe((tabu: TabuDTO) => {
-      if (tabu) {
-        const formGroup = this.createTabuForm(tabu);
-        this.tabuForm = formGroup;
-      }
-    });
-  }
-
- private updateProjectDates() {
-  this.tabuForm.patchValue({
-    dateWinningTender: this.formatDate(this.tabuForm.get('dateWinningTender')?.value),
-    developmentPeriodEndDate: this.formatDate(this.tabuForm.get('developmentPeriodEndDate')?.value),
-    collectionExpensesFrom1: this.formatDate(this.tabuForm.get('collectionExpensesFrom1')?.value),
-    collectionExpensesFrom2: this.formatDate(this.tabuForm.get('collectionExpensesFrom2')?.value),
-    collectionExpensesFrom3: this.formatDate(this.tabuForm.get('collectionExpensesFrom3')?.value),
-    insertDate: this.formatDate(new Date()),
-    updateDate: this.formatDate(new Date()),
-    hachiraContractEndDate: this.formatDate(this.tabuForm.get('hachiraContractEndDate')?.value)
+  this.srvTabu.GetTabuByApartmentId(this.apartmentId).subscribe((tabu: TabuDTO) => {
+    if (tabu) {
+      this.tabuForm = this.createTabuForm(tabu);
+    } else {
+      this.tabuForm = this.createTabuForm(this.getEmptyTabu());
+    }
   });
 }
 
-    private formatDate(date: any): string {
+  private updateProjectDates() {
+    this.tabuForm.patchValue({
+      dateWinningTender: this.formatDate(this.tabuForm.get('dateWinningTender')?.value),
+      developmentPeriodEndDate: this.formatDate(this.tabuForm.get('developmentPeriodEndDate')?.value),
+      collectionExpensesFrom1: this.formatDate(this.tabuForm.get('collectionExpensesFrom1')?.value),
+      collectionExpensesFrom2: this.formatDate(this.tabuForm.get('collectionExpensesFrom2')?.value),
+      collectionExpensesFrom3: this.formatDate(this.tabuForm.get('collectionExpensesFrom3')?.value),
+      insertDate: this.formatDate(new Date()),
+      updateDate: this.formatDate(new Date()),
+      hachiraContractEndDate: this.formatDate(this.tabuForm.get('hachiraContractEndDate')?.value)
+    });
+  }
+
+  private formatDate(date: any): string {
     if (!date) return ''; // בדיקה אם התאריך אינו תקף
     if (typeof date === 'string') {
       const [day, month, year] = date.split('-');
@@ -98,6 +155,24 @@ export class TabuComponent {
     }
     if (date.toString() === 'Invalid Date') return ''; // בדיקה אם התאריך אינו תקף לאחר ההמרה
     return date.toISOString().split('T')[0];
+  }
+  updateLabels(): void {
+    const form = this.tabuForm.value;
+    this.isMortgagePaid = form.isMortgagePaid;
+    this.isPurchaseTax = form.isPurchaseTax;
+    this.isPurchaseTaxValid = form.isPurchaseTaxValid;
+    this.isCapitalTax = form.isCapitalTax;
+    this.isCapitalTaxValid = form.isCapitalTaxValid;
+    this.isApprovalNeededSaleTaxHas = form.isApprovalNeededSaleTaxHas;
+    this.isApprovalNeededSaleTaxValid = form.isApprovalNeededSaleTaxValid;
+    this.isMunicipalityApproval = form.isMunicipalityApproval;
+    this.isMunicipalityApprovalValidity = form.isMunicipalityApprovalValidity;
+    this.isTaxApprovedTransferRightsSource = form.isTaxApprovedTransferRightsSource;
+    this.isTaxApprovedTransferRightsValid = form.isTaxApprovedTransferRightsValid;
+    this.isPartiesSigned = form.isPartiesSigned;
+    this.isSignedByMm = form.isSignedByMm;
+    this.isMortgageBillReceived = form.isMortgageBillReceived;
+
   }
 
   saveTabuChanges() {
